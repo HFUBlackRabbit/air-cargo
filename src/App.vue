7d1f4670bfe37344f6fcaa66d1cfd16f7e0fe8ef
BlackRabbit <hfu_blackrabbit@outlook.com>
2022-03-25 12:38:23 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+@import './assets/scss/style.scss';
+@font-face {
+  font-family: "PT Mono";
+  src: url("./assets/fonts/PTMono-Regular.ttf");
+}
+#app {
+  font-family: PT Mono, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
